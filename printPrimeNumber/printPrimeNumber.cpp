90fea3773c3e124432db614fc11e51bf1c3d89e8
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-02-21 15:36:59 +0800

    输出100以内的素数

--- printPrimeNumber/printPrimeNumber.cpp
+++ printPrimeNumber/printPrimeNumber.cpp
@@ -2,10 +2,40 @@
 //
 
 #include "stdafx.h"
+#include "stdlib.h"
+#include "iostream"
 
+using namespace std;
+
+int checkPrimeNumber(int n) {
+
+	if (n < 2*2) return 1;
+
+	for (int j = 2; j <= sqrt(n); j++) {
+
+		if (n % j == 0) {
+			return 0;
+		}
+	}
+	return 1;
+
+}
 
 int main()
 {
+	int totalNum = 100;
+
+	for (int i = 1; i < totalNum; i++) {
+
+		int res = checkPrimeNumber(i);
+
+		if (res == 1) {
+			cout << "the number is:" << i << endl;
+		}
+	}
+
+	system("pause");
+
     return 0;
 }
 
